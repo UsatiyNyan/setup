f92ffd780080fc9dd69f6d05280059eab9f75791
UsatiyNyan <kirillarionov123456@gmail.com>
2020-09-11 12:18:34 +0300

    docker and foxy-ros

--- ros2.sh
+++ ros2.sh
@@ -1,19 +1,26 @@
-sudo apt update && sudo apt -y install curl gnupg2 lsb-release
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update && sudo apt install -y \
   build-essential \
   cmake \
   git \
+  libbullet-dev \
   python3-colcon-common-extensions \
+  python3-flake8 \
   python3-pip \
-  python-rosdep \
+  python3-pytest-cov \
+  python3-rosdep \
+  python3-setuptools \
   python3-vcstool \
   wget
 # install some pip packages needed for testing
 python3 -m pip install -U \
   argcomplete \
-  flake8 \
   flake8-blind-except \
   flake8-builtins \
   flake8-class-newline \
@@ -24,10 +31,7 @@ python3 -m pip install -U \
   flake8-quotes \
   pytest-repeat \
   pytest-rerunfailures \
-  pytest \
-  pytest-cov \
-  pytest-runner \
-  setuptools
+  pytest
 # install Fast-RTPS dependencies
 sudo apt install --no-install-recommends -y \
   libasio-dev \
@@ -35,8 +39,5 @@ sudo apt install --no-install-recommends -y \
 # install Cyclone DDS dependencies
 sudo apt install --no-install-recommends -y \
   libcunit1-dev
+sudo apt install -y ros-foxy-desktop
 
-sudo apt -y install ros-eloquent-desktop
-sudo apt -y autoremove
-sudo apt -y update
-sudo apt -y upgrade
